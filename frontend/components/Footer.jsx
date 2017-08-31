b4d0fcba720a6d71eae71032e9b50e8148e26a5b
Noah Kang <noahskang@gmail.com>
2017-08-31 15:53:12 -0700

    add links to portfolio and github

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <div className="footer">
       <h1>Copyright © 2017 Impetus. All rights reserved.</h1>
+      <ul className="icons">
+				<li><a href="https://www.linkedin.com/in/noah-kang-31546166/" className="icon alt fa fa-linkedin"/></li>
+				<li><a href="https://github.com/noahskang/Impetus" className="icon alt fa fa-github"/></li>
+				<li><a href="https://www.facebook.com/noah.kang.54"/></li>
+			</ul>
     </div>
   );
 };
